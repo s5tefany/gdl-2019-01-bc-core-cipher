51aefe330c20f177e46cde3c54098893d9790454
s5tefany <m.garciac11@gmail.com>
2019-01-29 13:25:23 -0600

    logica encriptar funcionando

--- src/cipher.js
+++ src/cipher.js
@@ -3,33 +3,28 @@
   const n=1;  // desplazamiento
   //const divCipher=cipher.split(""); //Muestra ["A", "B", "C"] --->console.log(divCipher)
   //Muestra A B C --->console.log(divCipher[i]);
-  const converter=(cipher, n)=> {
+
+  const encode=(cipher, n)=> {
+  let letra="";
   for(let i=0; i<cipher.length; i++){ //Muestra 0 1 2 --->console.log(i);
       let numA=cipher.charCodeAt(i); // Muestra 65 66 67 --->  console.log(numA);
       let cipherLetters=(numA-65+n)%26+65; // Muestra 66 67 68 ---> console.log(cipherLetters) 
-      let letra=String.fromCharCode(cipherLetters); //Muestra B C D --->
-      console.log(letra);   
+      letra+=String.fromCharCode(cipherLetters); //Muestra B C D --->console.log(letra);
        }
+  return letra;
     }
-    
-  console.log(converter(cipher,n));
-  //prueba gitdavavsva
-   
-  //}
-  //console.log(resultado);
-  //console.log(resultado.join(""));
-  
-  
-  
+  console.log(encode(cipher,n));
   
   /*
-  function transformer(numASCII, n){
-    for(i=0; i<=cipher.length-1; i++){
-      let numASCII=cipher.charCodeAt(i); // Muestra 65 66 67 --->
-      console.log(numASCII)
-      let cipherLetters=(numASCII-65+n)%26+65; // Muestra 66 67 68 ---> console.log(cipherLetters) 
-      let letra=String.fromCharCode(cipherLetters); //Muestra B C D ---> console.log(letra);
+  const decipher="BCD"
+  const n=1;  // desplazamiento
+  const encode=(cipher, n)=> {
+  let letra="";
+  for(let i=0; i<cipher.length; i++){ //Muestra 0 1 2 --->console.log(i);
+      let numA=cipher.charCodeAt(i); // Muestra 65 66 67 --->  console.log(numA);
+      let cipherLetters=(numA-65+n)%26+65; // Muestra 66 67 68 ---> console.log(cipherLetters) 
+      letra+=String.fromCharCode(cipherLetters); //Muestra B C D --->console.log(letra);
+       }
+  return letra;
     }
-  } 
-  var resultado=divCipher.map(transformer);
-  console.log(resultado); */
+  console.log(encode(cipher,n));*/
