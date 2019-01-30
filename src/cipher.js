4a61ea17d437d29d7420ef1fe44ba32abc1ac0ba
s5tefany <m.garciac11@gmail.com>
2019-01-30 16:49:30 -0600

    0 errores no errors found, 2 failing al retornar

--- src/cipher.js
+++ src/cipher.js
@@ -1,31 +1,23 @@
- //window.cipher = {
-  const cipher="ABC"; //palabra a descifrar
-  const n=1;  // desplazamiento
-  //const divCipher=cipher.split(""); //Muestra ["A", "B", "C"] --->console.log(divCipher)
-  //Muestra A B C --->console.log(divCipher[i]);
-
-  const encode=(cipher, n)=> {
-  let letra="";
-  for(let i=0; i<cipher.length; i++){ //Muestra 0 1 2 --->console.log(i);
-      let numA=cipher.charCodeAt(i); // Muestra 65 66 67 --->  console.log(numA);
-      let cipherLetters=(numA-65+n)%26+65; // Muestra 66 67 68 ---> console.log(cipherLetters) 
-      letra+=String.fromCharCode(cipherLetters); //Muestra B C D --->console.log(letra);
-       }
-  return letra;
-    };
-  console.log(encode(cipher,n));
+window.cipher = {
   
+  encode: function cifrar (cipher, n) {
+    let letra="";
+    for(let i=0; i<cipher.length; i++){ //Muestra 0 1 2 --->console.log(i);
+        let numA=cipher.charCodeAt(i); // Muestra 65 66 67 --->  console.log(numA);
+        let cipherLetters=(numA-65+n)%26+65; // Muestra 66 67 68 ---> console.log(cipherLetters) 
+        letra+=String.fromCharCode(cipherLetters); //Muestra B C D --->console.log(letra);
+      }
+     
+    return letra;
+  },
   
-  const decipher="BCD";
-  const m=30;  // desplazamiento
-
-  const decode=(decipher, m)=> {
-  let letra="";
-  for(let i=0; i<decipher.length; i++){ //Muestra 0 1 2 --->console.log(i);
-      let numAs=decipher.charCodeAt(i); // Muestra 65 66 67 --->  console.log(numA);
-      let decipherLetters= (numAs+65-m)%26+65; // Muestra 66 67 68 ---> console.log(cipherLetters) 
-      letra+=String.fromCharCode(decipherLetters); //Muestra B C D --->console.log(letra);
-       }
-  return letra;
-    };
-  console.log(decode(decipher,m));
+  decode: function descifrar (cipher, n) {
+    let letra="";
+    for(let i=0; i<cipher.length; i++){ //Muestra 0 1 2 --->console.log(i);
+        let numAs=cipher.charCodeAt(i); // Muestra 65 66 67 --->  console.log(numA);
+        let decipherLetters= (numAs+65-n)%26+65; // Muestra 66 67 68 ---> console.log(cipherLetters) 
+        letra+=String.fromCharCode(decipherLetters); //Muestra B C D --->console.log(letra);
+      }
+    return letra;
+    },
+  };
--- src/index.js
+++ src/index.js
@@ -1,2 +1,18 @@
+function encriptar(){
+    let cipher = document.getElementById("string").value.toUpperCase();//ok
+    let n=Number(document.getElementById("offset").value);//ok
+    let mensaje = window.cipher.encode(cipher,n);
+    document.getElementById("result").innerHTML=mensaje;
+}
+document.getElementById("cipherButton").addEventListener("click", encriptar);
+
+function desencriptar(){
+    let cipher = document.getElementById("string").value.toUpperCase();
+    let n=Number(document.getElementById("offset").value);
+    let mensaje = window.cipher.decode(cipher,n);
+    document.getElementById("result").innerHTML=mensaje;
+}
+
+document.getElementById("decipherButton").addEventListener("click",desencriptar);
 
 
