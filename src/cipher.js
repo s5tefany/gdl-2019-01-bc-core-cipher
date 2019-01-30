1073c6b4af1d8ab2d7e899139f9fea437d0cbd71
s5tefany <m.garciac11@gmail.com>
2019-01-30 10:23:57 -0600

    cifrar y descifrar funcionando, 0 errores found

--- src/cipher.js
+++ src/cipher.js
@@ -1,4 +1,4 @@
-//window.cipher = {
+ //window.cipher = {
   const cipher="ABC"; //palabra a descifrar
   const n=1;  // desplazamiento
   //const divCipher=cipher.split(""); //Muestra ["A", "B", "C"] --->console.log(divCipher)
@@ -12,19 +12,20 @@
       letra+=String.fromCharCode(cipherLetters); //Muestra B C D --->console.log(letra);
        }
   return letra;
-    }
+    };
   console.log(encode(cipher,n));
   
-  /*
-  const decipher="BCD"
-  const n=1;  // desplazamiento
-  const encode=(cipher, n)=> {
+  
+  const decipher="BCD";
+  const m=30;  // desplazamiento
+
+  const decode=(decipher, m)=> {
   let letra="";
-  for(let i=0; i<cipher.length; i++){ //Muestra 0 1 2 --->console.log(i);
-      let numA=cipher.charCodeAt(i); // Muestra 65 66 67 --->  console.log(numA);
-      let cipherLetters=(numA-65+n)%26+65; // Muestra 66 67 68 ---> console.log(cipherLetters) 
-      letra+=String.fromCharCode(cipherLetters); //Muestra B C D --->console.log(letra);
+  for(let i=0; i<decipher.length; i++){ //Muestra 0 1 2 --->console.log(i);
+      let numAs=decipher.charCodeAt(i); // Muestra 65 66 67 --->  console.log(numA);
+      let decipherLetters= (numAs+65-m)%26+65; // Muestra 66 67 68 ---> console.log(cipherLetters) 
+      letra+=String.fromCharCode(decipherLetters); //Muestra B C D --->console.log(letra);
        }
   return letra;
-    }
-  console.log(encode(cipher,n));*/
+    };
+  console.log(decode(decipher,m));
