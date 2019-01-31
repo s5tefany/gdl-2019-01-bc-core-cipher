8d23f333b3dec2cbe6eda9dd7dd48ee42df91d3d
s5tefany <m.garciac11@gmail.com>
2019-01-31 13:15:24 -0600

    pruebas pasadas, archivos enlazados

--- src/cipher.js
+++ src/cipher.js
@@ -1,17 +1,16 @@
 window.cipher = {
   
-  encode: function cifrar (cipher, n) {
+  encode: function cifrar (n, cipher) {
     let letra="";
     for(let i=0; i<cipher.length; i++){ //Muestra 0 1 2 --->console.log(i);
         let numA=cipher.charCodeAt(i); // Muestra 65 66 67 --->  console.log(numA);
-        let cipherLetters=(numA-65+n)%26+65; // Muestra 66 67 68 ---> console.log(cipherLetters) 
+        let cipherLetters=(numA-65+n)%26+65; // Muestra 66 67 68 ---> console.log(cipherLetters);
         letra+=String.fromCharCode(cipherLetters); //Muestra B C D --->console.log(letra);
       }
-     
-    return letra;
+     return letra;
   },
   
-  decode: function descifrar (cipher, n) {
+  decode: function descifrar (n, cipher) {
     let letra="";
     for(let i=0; i<cipher.length; i++){ //Muestra 0 1 2 --->console.log(i);
         let numAs=cipher.charCodeAt(i); // Muestra 65 66 67 --->  console.log(numA);
--- src/index.js
+++ src/index.js
@@ -1,18 +1,19 @@
 function encriptar(){
-    let cipher = document.getElementById("string").value.toUpperCase();//ok
-    let n=Number(document.getElementById("offset").value);//ok
-    let mensaje = window.cipher.encode(cipher,n);
-    document.getElementById("result").innerHTML=mensaje;
+    let cipher = document.getElementById("string").value.toUpperCase();
+    let n=Number(document.getElementById("offset").value);
+    //let mensaje = window.cipher.encode(n, cipher);
+    document.getElementById("result").value=window.cipher.encode(n, cipher);
 }
+
 document.getElementById("cipherButton").addEventListener("click", encriptar);
 
 function desencriptar(){
     let cipher = document.getElementById("string").value.toUpperCase();
     let n=Number(document.getElementById("offset").value);
-    let mensaje = window.cipher.decode(cipher,n);
-    document.getElementById("result").innerHTML=mensaje;
+    //let mensaje = window.cipher.decode(n, cipher);
+    document.getElementById("result").value=window.cipher.decode(n, cipher);
 }
 
-document.getElementById("decipherButton").addEventListener("click",desencriptar);
+document.getElementById("decipherButton").addEventListener("click", desencriptar);
 
 
